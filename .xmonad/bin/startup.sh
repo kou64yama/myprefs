2fd13d92bb81f1fa2bb7159ab2dfd9da17e5211b
Koji Yamada <kou64yama@gmail.com>
2012-08-29 15:37:12 +0900

    背景を変更

--- .xmonad/bin/startup.sh
+++ .xmonad/bin/startup.sh
@@ -1,6 +1,5 @@
-#!/bin/zsh
+#!/bin/sh
 
-#source ~/.zsh.d/60-util.sh
-#hsetroot -fill $(random-list $(find "${HOME}/.wallpapers" -follow -type f -iname "*.jpg" -or -iname "*.png"))
-hsetroot -fill ~/Dropbox/Photos/wallpapers/Opera-Background-Speaker.jpg
+#hsetroot -fill ~/Dropbox/Photos/wallpapers/Opera-Background-Speaker.jpg
+hsetroot -fill /usr/share/backgrounds/gentoo10/wider/1920x1080/blue.jpg
 xcompmgr &
