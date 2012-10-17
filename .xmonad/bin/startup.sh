39105547d5d848e858aa330a0707daed84a7c55d
Koji Yamada <yamada@marbo.knc.info.gifu-u.ac.jp>
2012-10-18 04:18:01 +0900

    Changed keyboard layout.

--- .xmonad/bin/startup.sh
+++ .xmonad/bin/startup.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
 
-#hsetroot -fill ~/Dropbox/Photos/wallpapers/Opera-Background-Speaker.jpg
-setxkbmap -option ctrl:swapcaps
 hsetroot -fill /usr/share/backgrounds/gentoo10/wider/1920x1080/blue.jpg
 xcompmgr &
