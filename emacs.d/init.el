47907a34dcfbcd67b2006290a2f239ee4ec154fe
Koji YAMADA <kou64yama@gmail.com>
2013-05-09 05:30:04 +0900

    空白文字の表示を変更

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -27,7 +27,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(global-whitespace-mode t)
  '(global-linum-mode t)
  '(linum-number-mode t)
  '(column-number-mode t)
@@ -68,6 +67,14 @@
   (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
   (package-initialize)
 )
+
+(when (require 'whitespace nil t)
+  (custom-set-variables
+   '(global-whitespace-mode t)
+   '(whitespace-style '(face tabs tab-mark spaces space-mark))
+   )
+)
+
 
 ;; anything
 ;;
